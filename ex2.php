6c702fafc0e3d322e38610480e88704ffd4d031a
Sarah Isabelly <SARAHISABELLY28@GMAIL.COM>
2025-08-10 23:22:11 -0300

    Add files via upload

---
+++ ex2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex 2</title>
+</head>
+<body>
+    <?php
+    $n1 = 10;
+    $n2 = 20;
+    $soma = $n1 + $n2;
+
+    echo "A soma entre $n1 e $n2 é: " . $soma;
+    ?>
+</body>
+</html>
